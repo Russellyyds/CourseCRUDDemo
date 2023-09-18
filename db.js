709dbbc2faa0ff08acbbe6fbb3a76e583a1088a0
Russellyyds <954013384@qq.com>
2023-09-18 23:05:54 +1000

    add some comment for db URL

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
-const db="mongodb+srv://russell1997100188:7tPDb0vHGWB3KNAR@cluster0.pejqq7y.mongodb.net/course";
-const mongose=require("mongoose");
+//you can change the below URL to yours.
 
+const mongose=require("mongoose");
+const db="mongodb+srv://russell1997100188:7tPDb0vHGWB3KNAR@cluster0.pejqq7y.mongodb.net/course";
 
 const connectDB=async()=>{
     try {
